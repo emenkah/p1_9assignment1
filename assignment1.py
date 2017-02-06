dc4b3fac74c269d12a1f1e8ba2633fbd01461ffd
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 00:17:16 +0100

    List pupolation implemented

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,19 @@
 import sys
 cmd = sys.argv[1]
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+def func(arg):
+    xval = np.arange(-5.0, 5.0, 0.1)
+    f = lambda x:x
+ 
+    if arg == '1':
+        yval = f(xval)
+        print yval
+    else:
+        print 'yval would be populated by a diff function'
+
+
+
+test = func(cmd)
