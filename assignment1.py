1cd737eec57d6993668faf9be6916346e44de3ee
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 02:32:28 +0100

    Additional functions of x squred and cude implemented

--- assignment1.py
+++ assignment1.py
@@ -9,8 +9,18 @@ def func(arg):
     f = lambda x:x
  
     if arg == '1':
+    	f = lambda x:x
         yval = f(xval)
         #print yval
+
+    elif arg == '2':
+    	f = lambda x:x**2
+        yval = f(xval)
+
+    elif arg == '3':
+    	f = lambda x:x**3
+        yval = f(xval)
+
     else:
         print 'yval would be populated by a diff function'
 	
