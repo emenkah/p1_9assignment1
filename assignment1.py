993d58a436724c4a0b4f04a62694e5a18ab976df
Elliot S. Menkah <elliotsmenkah@gmail.com>
2017-02-07 10:14:26 +0100

    Added Trignometric functions

--- assignment1.py
+++ assignment1.py
@@ -6,10 +6,19 @@ import matplotlib.pyplot as plt
 
 def func(arg):
     xval = np.arange(-5.0, 5.0, 0.1)
-    f = lambda x:x
  
     if arg == '1':
+        f = lambda x:x
         yval = f(xval)
+        #print yval
+    elif arg == '2':
+        yval = np.sin(xval) 
+        #print yval
+    elif arg == '3':
+        yval = np.cos(xval)
+        #print yval
+    elif arg == '4':
+        yval = np.tan(xval)
         #print yval
     else:
         print 'yval would be populated by a diff function'
